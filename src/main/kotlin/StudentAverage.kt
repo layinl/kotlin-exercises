b32d8cd257ad6449fe748ba5b30a63b5ee3b4b89
layinl <layon.final008@gmail.com>
2023-11-17 21:26:19 -0300

    feat: added input validation and try catch

--- src/main/kotlin/StudentAverage.kt
+++ src/main/kotlin/StudentAverage.kt
@@ -1,8 +1,17 @@
 fun main() {
+  try {
+    println("From 0 to 10, what is the student average?")
     val studentAverage = readLine()!!.toDouble()
-    when {
-        studentAverage < 5 -> println("REP")
-        studentAverage >= 5 && studentAverage < 7 -> println("REC")
-        studentAverage >= 7 -> println("APR")
-    }
+    println(
+      when {
+        studentAverage in 0.0..4.9 -> "FAIL"
+        studentAverage < 7 -> "REC"
+        studentAverage in 7.0..10.0 -> "APR"
+        else -> "Invalid value"
+      }
+    )
+  } catch (e: NumberFormatException) {
+    println("Please input a number")
+    e.printStackTrace()
+  }
 }
