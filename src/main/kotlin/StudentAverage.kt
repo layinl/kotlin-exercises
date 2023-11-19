81f0283097e7ea9ae1235267eb750aad55bbd603
layinl <layon.final008@gmail.com>
2023-11-18 21:24:02 -0300

    feat: changed to a better output and added docs

--- src/main/kotlin/StudentAverage.kt
+++ src/main/kotlin/StudentAverage.kt
@@ -1,12 +1,20 @@
+/**
+ * ## Student Average Program
+ *
+ * This program gets the student's average in a 0-10 scale and outputs if
+ * he **failed**, needs **Grade Recovery** or **Passed**, based on Brazil's
+ * Ministry of Education grade system (...sort of).
+ * @throws NumberFormatException if the value is Not a Number
+ */
 fun main() {
   try {
     println("From 0 to 10, what is the student average?")
     val studentAverage = readLine()!!.toDouble()
     println(
       when {
-        studentAverage in 0.0..4.9 -> "FAIL"
-        studentAverage < 7 -> "REC"
-        studentAverage in 7.0..10.0 -> "APR"
+        studentAverage in 0.0..4.9 -> "FAILED"
+        studentAverage < 7 -> "GRADE RECOVERY"
+        studentAverage in 7.0..10.0 -> "PASSED"
         else -> "Invalid value"
       }
     )
